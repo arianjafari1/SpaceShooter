d57e89c79509b0bdda3f03a5815861e82ea88992
arianjafari1 <arian.jafari@outlook.com>
2022-10-28 07:26:40 +0100

    C++ class: finished my C++ SpinComponent, and it's ready to be added to an Actor in the Editor

--- Source/SpaceShooter/Private/SpinComponent.cpp
+++ Source/SpaceShooter/Private/SpinComponent.cpp
@@ -19,6 +19,8 @@ void USpinComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
+	StartLocation = GetOwner()->GetActorLocation(); // the start location is equal to the location of the actor the component is attached to
+
 	// ...
 	
 }
@@ -28,6 +30,14 @@ void USpinComponent::BeginPlay()
 void USpinComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	GetOwner()->AddActorWorldRotation(Spin * DeltaTime); //add spin to the world rotation for this actor and multiply by delta time, so it's frame-rate independent
+
+	float Time = GetWorld()->GetRealTimeSeconds(); //getting world time and assigning it to the float variable Time
+	
+	float Sine = FMath::Sin(Time * ElevationSpeed); //using Sin function from the FMath library of Unreal, to make elevation system work
+	
+	GetOwner()->SetActorLocation(StartLocation + (Elevation * Sine)); //setting the actor location, and adding the elevetion on the tick, so it keeps going up and down
 
 	// ...
 }
--- Source/SpaceShooter/Public/SpinComponent.h
+++ Source/SpaceShooter/Public/SpinComponent.h
@@ -24,5 +24,15 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Settings") //exposing the variable to the editor for read and write under the category settings
+	FRotator Spin; //the rotation of the actor that the component will be attached to
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Settings")
+	FVector Elevation; //vector made visible in the editor to manipulate elevation of actor
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Settings")
+	float ElevationSpeed; //speed at which the elevation will happen, when it goes up and down
+
+private:
+	FVector StartLocation; //vector that will use to get the location of the actor that its attached to
 };
